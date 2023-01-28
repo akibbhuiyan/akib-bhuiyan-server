f7a781561b8cd8c17be2350769862d69a3340ee0
akibbhuiyan <akibbhh@gmail.com>
2023-01-28 18:25:25 +0600

    async

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
-const express = require('express')
-const cors = require('cors')
-var bodyParser = require('body-parser')
-const nodemailer = require("nodemailer")
-const app = express()
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.get('/', (req, res) => {
-    res.send('Personal PortFolio Server!')
-})
-
-app.post('/contact', async (req, res) => {
-    const name = req.body.firstName + req.body.lastName;
-    const email = req.body.email;
-    const message = req.body.message;
-    const phone = req.body.phone;
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.USER, // generated ethereal user
-            pass: process.env.PASS, // generated ethereal password
-        },
+const express = require("express");
+const cors = require("cors");
+var bodyParser = require("body-parser");
+const nodemailer = require("nodem    ailer");
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+async function run() {
+  try {
+    app.get("/", (req, res) => {
+      res.send("Personal PortFolio Server!");
     });
-    const mail = {
+
+    app.post("/contact", async (req, res) => {
+      const name = req.body.firstName + req.body.lastName;
+      const email = req.body.email;
+      const message = req.body.message;
+      const phone = req.body.phone;
+      let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.USER, // generated ethereal user
+          pass: process.env.PASS, // generated ethereal password
+        },
+      });
+      const mail = {
         from: `${name}`,
-        to: 'alexboss00852@gmail.com',
+        to: "alexboss00852@gmail.com",
         subject: "Contact Form Submission - PortFolio",
-        html:
-            ` <p> Name: ${name}</p>
+        html: ` <p> Name: ${name}</p>
         <p> Email: ${email}</p>
         <p> Phone: ${phone}</p>
-        <p> Message: ${message}</p>`
-
-    }
-    await transporter.sendMail(mail, (err, document) => {
+        <p> Message: ${message}</p>`,
+      };
+      await transporter.sendMail(mail, (err, document) => {
         if (err) {
-            res.json(err)
+          res.json(err);
         } else {
-            res.send(document)
+          res.send(document);
         }
+      });
     });
-
-
-})
-app.listen(process.env.PORT || 5000)
+  } finally {
+  }
+}
+run().catch(console.log);
+app.listen(process.env.PORT || 5000);
