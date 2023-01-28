ae356aec6850b9ff2d5687ea5463fa57f55297ab
akibbhuiyan <akibbhh@gmail.com>
2023-01-28 18:20:31 +0600

    server added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Personal PortFolio Server!')
 })
 
 app.post('/contact', async (req, res) => {
