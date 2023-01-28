9b97903403557e17f2ec5da2bea079b5292f80ab
akibbhuiyan <akibbhh@gmail.com>
2023-01-28 19:37:00 +0600

    ss

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
 const express = require("express");
 const cors = require("cors");
 var bodyParser = require("body-parser");
-const nodemailer = require("nodem    ailer");
+const nodemailer = require("nodemailer");
 const app = express();
+require("dotenv").config();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Personal PortFolio Server!");
 });
-// async function run() {
-//   try {
-//     app.post("/contact", async (req, res) => {
-//       const name = req.body.firstName + req.body.lastName;
-//       const email = req.body.email;
-//       const message = req.body.message;
-//       const phone = req.body.phone;
-//       let transporter = nodemailer.createTransport({
-//         service: "gmail",
-//         auth: {
-//           user: process.env.USER, // generated ethereal user
-//           pass: process.env.PASS, // generated ethereal password
-//         },
-//       });
-//       const mail = {
-//         from: `${name}`,
-//         to: "alexboss00852@gmail.com",
-//         subject: "Contact Form Submission - PortFolio",
-//         html: ` <p> Name: ${name}</p>
-//         <p> Email: ${email}</p>
-//         <p> Phone: ${phone}</p>
-//         <p> Message: ${message}</p>`,
-//       };
-//       await transporter.sendMail(mail, (err, document) => {
-//         if (err) {
-//           res.json(err);
-//         } else {
-//           res.send(document);
-//         }
-//       });
-//     });
-//   } finally {
-//   }
-// }
-// run().catch(console.log);
+async function run() {
+  try {
+    app.post("/contact", async (req, res) => {
+      const name = req.body.firstName + req.body.lastName;
+      const email = req.body.email;
+      const message = req.body.message;
+      const phone = req.body.phone;
+      let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.USER, // generated ethereal user
+          pass: process.env.PASS, // generated ethereal password
+        },
+      });
+      const mail = {
+        from: `${name}`,
+        to: "alexboss00852@gmail.com",
+        subject: "Contact Form Submission - PortFolio",
+        html: ` <p> Name: ${name}</p>
+        <p> Email: ${email}</p>
+        <p> Phone: ${phone}</p>
+        <p> Message: ${message}</p>`,
+      };
+      await transporter.sendMail(mail, (err, document) => {
+        if (err) {
+          res.json(err);
+        } else {
+          res.send(document);
+        }
+      });
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
 app.listen(process.env.PORT || 5000);
